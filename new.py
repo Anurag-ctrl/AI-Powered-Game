977a4062681de0a17dfafe1ba4108e7af8060bb6
Anurag Kumar Verma <73578523+Anurag-ctrl@users.noreply.github.com>
2020-10-28 22:43:12 +0530

    Delete new.py

--- new.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as pn
-import random as rd
-import matplotlib.pyplot as mlp
-print("hello world")
-print(random.randint(1:10))
-print('Hello anurag')
-print("hello papa")

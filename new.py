a23eccfb2b2694c39c0b3c954b36f5413931b41b
Anurag Kumar Verma <73578523+Anurag-ctrl@users.noreply.github.com>
2020-10-28 21:44:27 +0530

    Update new.py

--- new.py
+++ new.py
@@ -3,6 +3,6 @@ import numpy as pn
 import random as rd
 import matplotlib.pyplot as mlp
 print("hello world")
-print(random.randint(1:10))
+print(random.randint(1,10))
 print('Hello anurag')
 print("hello papa")

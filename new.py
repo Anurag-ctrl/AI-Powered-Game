855ff142995f8548ac4c863cb5680f13a1688971
Anurag Kumar Verma <deepak.verma7485@gmail.com>
2020-10-30 23:05:39 +0530

    Initial Commit

--- new.py
+++ new.py
@@ -1,16 +1,45 @@
 import random
+import time
 cs=0
 ps=0
 list = [1,2]
-a=input("Enter a number\n")
+#Take main input
 while True:
-    if a not in list:
-        input("Enter a number\n")
+    a=input("Enter a number\n")
+    #If the input goes wrong then demand a correct one in contini...
+    while True:
+        if a not in list:
+            input('Invalid Entry !')
+            input("Enter a number")
+        else:
+            break
+    #Real game begins and the score start calculated
+    if a == random.randint(1,2):
+        cs=cs+1
+        print("Player Score =",ps,"\n","Computer Score =",cs)
     else:
+        ps +=1
+        print("Player Score =", ps, "\n", "Computer Score =", cs)
+    #If either the score has reached the half point that is 5 then half-time will start
+    #practically two half time will be there in our game. of each 60 sec(1min)
+    if ps==5:
+        print('Half Time')
+        time.sleep(55)
+        print('Game About to Begin')
+        for t in range(5,0,-1):
+            time.sleep(1)
+            print('Game Starting in',t,'Seconds..')
+    elif cs==5:
+        print('Half Time')
+        time.sleep(60)
+        print('Game About to Begin')
+        for t in range(5, 0, -1):
+            time.sleep(1)
+            print('Game Starting in', t, 'Seconds..')
+    #Final Results
+    if ps==10:
+        print('Player Won')
         break
-if a == random.randint(1,2):
-    cs=cs+1
-    print("Player Score =",ps,"\n","Computer Score =",cs)
-else:
-    ps +=1
-    print("Player Score =", ps, "\n", "Computer Score =", cs)
+    elif cs==10:
+        print('Computer Won')
+        break
